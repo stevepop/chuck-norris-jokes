7bc0caf54359095b4cb7bb1fa513cb67b1cca74e
Steve <stevepop@gmail.com>
2019-06-24 09:37:20 +0100

    Setup package and initial tests

---
+++ tests/JokeFactoryTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Stevepop\ChuckNorrisJokes\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Stevepop\ChuckNorrisJokes\JokeFactory;
+
+class JokeFactoryTest extends TestCase
+{
+	/** @test **/
+    public function it_returns_a_random_joke()
+    {
+        $jokes = new JokeFactory([
+            'This is a joke',
+        ]);
+
+        $joke = $jokes->getRandomJoke();
+
+        $this->assertSame('This is a joke', $joke);
+    }
+
+    /** @test **/
+    public function it_returns_a_predefined_joke()
+    {
+        $chuckNorrisJokes = [
+            'Chuck Norris\' tears cure cancer. Too bad he has never cried.',
+            'Chuck Norris counted to infinity... Twice.',
+            'If you can see Chuck Norris, he can see you. If you can\'t see Chuck Norris you may be only seconds away from death.',
+
+        ];
+
+        $jokes = new JokeFactory();
+
+        $joke = $jokes->getRandomJoke();
+
+        $this->assertContains($joke, $chuckNorrisJokes);
+    }
+}
