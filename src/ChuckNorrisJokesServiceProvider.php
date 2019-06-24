fb3d9802270ceeb2ee1a0dce2fd322d1b9f7b06b
Steve <stevepop@gmail.com>
2019-06-24 21:14:49 +0100

    Added Artisan command

--- src/ChuckNorrisJokesServiceProvider.php
+++ src/ChuckNorrisJokesServiceProvider.php
@@ -3,11 +3,17 @@
 namespace Stevepop\ChuckNorrisJokes;
 
 use Illuminate\Support\ServiceProvider;
+use Stevepop\ChuckNorrisJokes\Console\ChuckNorrisJoke;
 
 class ChuckNorrisJokesServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                ChuckNorrisJoke::class
+            ]);
+        }
     }
 
     public function register()
---
+++ src/Console/ChuckNorrisJoke.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace Stevepop\ChuckNorrisJokes\Console;
+
+
+use Illuminate\Console\Command;
+use Stevepop\ChuckNorrisJokes\Facades\ChuckNorris;
+
+class ChuckNorrisJoke extends Command
+{
+    protected $signature = 'chuck-norris';
+
+
+    protected $description = 'Output a funny Chuck Norris joke.';
+
+
+    public function handle()
+    {
+        $this->info(ChuckNorris::getRandomJoke());
+    }
+}
